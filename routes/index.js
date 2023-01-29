8ddcff525f0d2f3c614bc7c4b53b036b6a99d1e3
Daniel Romero <deromerov07@gmail.com>
2023-01-29 22:40:07 +0100

    lab almost-done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movie.model');
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+        .then((movies) => {
+            console.log(movies);
+            res.render('movies', { movies })
+        })
+        .catch((error) => next(error))
+})
+
+router.get('/movie/:id', (req, res, next) => {
+    Movie.find({_id: req.params.id})
+    .then((movie) => {
+        console.log(movie[0]);
+        res.render('movie', { movie: movie[0] })
+    })
+})
+
 
 module.exports = router;
